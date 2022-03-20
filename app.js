aad861af4aeab18da31fa97b6619ef4489ae7965
buihang <camhang190497@gmail.com>
2022-03-20 21:09:09 +0900

    xu li cac route khac nhau

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Alows the request to continue to the next middleware in line
+app.use('/', (req,res,next) => {
+    console.log('this always runs!');
+    next();
+});
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    
+    res.send('<h1>The "Add Product" Page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
     
     res.send('<h1>Hello from Express!</h1>');
